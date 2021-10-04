17478d47f82e455d8973e7f502679c683d584f9f
deCyma <soos0228@naver.com>
2021-10-04 09:55:28 +0000

    feat: hp train 분리

--- src/hyper_parameters.py
+++ src/hyper_parameters.py
@@ -1,3 +1,19 @@
+import torch
+import os
+import copy
+from importlib import import_module
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    Trainer,
+    TrainingArguments,
+)
+
+from src.validation import compute_metrics_f1
+from src.hyper_parameters import hp_space_sigopt
+from src.data_load import *
+from src.train import load_tokenizer_and_model, label_to_num
+
 # 클로저 쓰자
 def hp_space_sigopt(_):
     return [
@@ -21,3 +37,86 @@ def hp_space_sigopt(_):
             "type": "double",
         },
     ]
+
+
+# 하이퍼 파라미터 튜닝용 설정
+def set_hp_training_args(output_dir, train_args_cfg):
+    return TrainingArguments(
+        output_dir=output_dir,
+        evaluation_strategy=train_args_cfg.evaluation_strategy,  # evaluation strategy to adopt during training
+        eval_steps=train_args_cfg.eval_steps,  # evaluation step.
+        disable_tqdm=True,
+        load_best_model_at_end=True,
+        report_to="wandb",
+    )
+
+
+# 하이퍼 파라미터용 훈련
+def hyper_parameter_train(cfg):
+    train_name = cfg.name
+
+    # load model and tokenizer
+    MODEL_INDEX = cfg.model.pick
+    tokenizer, model = load_tokenizer_and_model(MODEL_INDEX, cfg.model)
+
+    # load dataset
+    dataset = train_data_with_addition(cfg.dir_path.train_data_path, cfg.dataset)
+    temp_dataset, train_dataset = get_stratified_K_fold(
+        dataset, cfg.hyperparameters.dataset.train
+    )
+    valid_cfg = cfg.hyperparameters.dataset.valid
+    valid_cfg.n_splits = int(
+        valid_cfg.n_splits
+        * (cfg.hyperparameters.dataset.train.n_splits - 1)
+        / cfg.hyperparameters.dataset.train.n_splits
+    )
+    _, valid_dataset = get_stratified_K_fold(temp_dataset, valid_cfg)
+    train_label = label_to_num(train_dataset["label"].values, cfg)
+    valid_label = label_to_num(valid_dataset["label"].values, cfg)
+
+    # tokenizing dataset
+    tokenizer_module = getattr(
+        import_module("src.tokenizing"), cfg.tokenizer.list[cfg.tokenizer.pick]
+    )
+    tokenized_train = tokenizer_module(train_dataset, tokenizer)
+    tokenized_valid = tokenizer_module(valid_dataset, tokenizer)
+
+    # make dataset for pytorch.
+    RE_train_dataset = RE_Dataset(tokenized_train, train_label)
+    RE_valid_dataset = RE_Dataset(tokenized_valid, valid_label)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    print(device)
+
+    # setting model hyperparameter
+    MODEL_NAME = cfg.model.model_list[MODEL_INDEX]
+    model_config = AutoConfig.from_pretrained(MODEL_NAME)
+    model_config.num_labels = cfg.model.num_labels
+    model = AutoModelForSequenceClassification.from_pretrained(
+        MODEL_NAME, config=model_config
+    )
+    model.resize_token_embeddings(len(tokenizer))
+
+    def model_init():
+        return copy.deepcopy(model)
+
+    output_dir = os.path.join(cfg.dir_path.base, train_name, "hp_optimize")
+
+    # Training 설정
+    training_args = set_hp_training_args(output_dir, cfg.train_args)
+    trainer = Trainer(
+        model_init=model_init,  # the instantiated 🤗 Transformers model to be trained
+        args=training_args,  # training arguments, defined above
+        train_dataset=RE_train_dataset,  # training dataset
+        eval_dataset=RE_valid_dataset,  # evaluation dataset
+        compute_metrics=compute_metrics_f1,  # define metrics function
+    )
+
+    # hp train model
+    trainer.hyperparameter_search(
+        direction="maximize",
+        backend=cfg.hyperparameters.backend,
+        hp_space=hp_space_sigopt,
+        n_trials=cfg.hyperparameters.n_trials,
+    )
