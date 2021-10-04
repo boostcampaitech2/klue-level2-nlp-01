c2d4f29a0097ef817c69e1b9d9425270ca5332cd
deCyma <soos0228@naver.com>
2021-10-04 21:30:18 +0000

    feat: 토크나이징 추가 (함수명 추천좀..)

--- src/tokenizing.py
+++ src/tokenizing.py
@@ -146,3 +146,42 @@ def tokenized_dataset_with_least_special_tokens(dataset, tokenizer):
         add_special_tokens=True,
     )
     return tokenized_sentences
+
+
+def data_preprocess2(dataset, tokenizer):
+    """typed-entity-marker"""
+    sp_tokens = set()
+    concat_entity = []
+    for sub, obj, sen in zip(
+        dataset["subject_entity"], dataset["object_entity"], dataset["sentence"]
+    ):
+        tokens = [
+            (sub["start_idx"], f"[S:{sub['type']}]"),
+            (sub["end_idx"] + 1, f"[/S:{sub['type']}]"),
+            (obj["start_idx"], f"[O:{obj['type']}]"),
+            (obj["end_idx"] + 1, f"[/O:{obj['type']}]"),
+        ]
+        sp_tokens.update(
+            [
+                f"[S:{sub['type']}]",
+                f"[/S:{sub['type']}]",
+                f"[O:{obj['type']}]",
+                f"[/O:{obj['type']}]",
+            ]
+        )
+        tokens.sort(key=lambda x: x[0], reverse=True)
+        for start_index, token in tokens:
+            sen = "".join([sen[:start_index], token, sen[start_index:]])
+        concat_entity.append(sen)
+    special_token = {"additional_special_tokens": list(sp_tokens)}
+    tokenizer.add_special_tokens(special_token)
+    return tokenizer(
+        # datasets["entity_span"],
+        concat_entity,
+        return_tensors="pt",
+        truncation=True,
+        padding="max_length",
+        max_length=256,
+        add_special_tokens=True,
+        return_token_type_ids=True,
+    )
