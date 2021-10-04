5fdee3ed91434d9f7dd04b2ad6e989a3083380fd
deCyma <soos0228@naver.com>
2021-10-04 02:57:41 +0000

    fix: hyperparameter시 inference 실행안하도록 변경

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from src.inference import inference_main as inference
 @hydra.main(config_path="conf", config_name="main")
 def main(cfg: DictConfig):
     train(cfg)
-    inference(cfg)
+    if not cfg.hyperparameters.set:
+        inference(cfg)
 
 
 if __name__ == "__main__":
