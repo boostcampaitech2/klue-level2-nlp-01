aef3027808fb052ba1cd79d3e412b643f182d1f6
decyma <soos0228@naver.com>
2021-10-05 11:13:58 +0000

    refactor: 전처리 함수 가독성 증가

--- src/pre_process.py
+++ src/pre_process.py
@@ -15,25 +15,15 @@ def preprocess_decorator(func):
         labels = []
         add_tokens = set()
         special_tokens = set()
-        AEDA = None
         for sub, obj, sen, label in zip(
             dataset["subject_entity"],
             dataset["object_entity"],
             dataset["sentence"],
             dataset["label"],
         ):
-            sens, lab, a_t, s_t = func(
-                sub,
-                obj,
-                sen,
-                label,
-                # set_AEDA,
-                False,
-                AEDA,
-                AEDA_cfg,
-            )
-            sentences.extend(sens)
-            labels.extend(lab)
+            sens, a_t, s_t = func(sub, obj, sen)
+            sentences.append(sens)
+            labels.append(label)
             add_tokens.update(a_t)
             special_tokens.update(s_t)
         if set_AEDA:
@@ -62,19 +52,10 @@ def preprocess_decorator(func):
 
 
 @preprocess_decorator
-def dataset_preprocess_base(
-    sub,
-    obj,
-    sen,
-    label,
-    set_AEDA=False,
-    AEDA=None,
-    AEDA_cfg=None,
-):
+def dataset_preprocess_base(sub, obj, sen):
     """
     sentence: 이순신@PER[SEP]장군@TITLE[SEP]@@@@@이순신 장군은 조선 제일의 무신이다.
     """
-    sentences, labels = [], []
     add_tokens, special_tokens = [], []
     pre_sen = (
         sub["word"]
@@ -87,29 +68,38 @@ def dataset_preprocess_base(
         + "@@@@@"
     )
     add_tokens.extend([sub["type"], obj["type"]])
-    sentences.append(pre_sen + sen)
-    labels.append(label)
-    if set_AEDA:
-        aug_sentences = AEDA_generator(AEDA, sen, AEDA_cfg.generator)
-        sentences.extend(aug_sentences)
-        labels.extend([label] * len(aug_sentences))
-    return sentences, labels, add_tokens, special_tokens
+    sentence = pre_sen + sen
+    return sentence, add_tokens, special_tokens
 
 
 @preprocess_decorator
-def dataset_preprocess_with_special_tokens(
-    sub,
-    obj,
-    sen,
-    label,
-    set_AEDA=False,
-    AEDA=None,
-    AEDA_cfg=None,
-):
+def dataset_preprocess_base_for_AEDA(sub, obj, sen):
+    """
+    sentence: 이순신@PER & 장군@TITLE @@@@@ 이순신 장군은 조선 제일의 무신이다.
+    @@@@@ 를 쓸 경우 토크나이저시 해당부분이 SEP으로 교체됩니다.
+    AEDA 시 좋은성능을 보일거라 예상합니다.
+    """
+    add_tokens, special_tokens = [], []
+    pre_sen = (
+        sub["word"]
+        + "@"
+        + sub["type"]
+        + " & "
+        + obj["word"]
+        + "@"
+        + obj["type"]
+        + "@@@@@"
+    )
+    add_tokens.extend([sub["type"], obj["type"]])
+    sentence = pre_sen + sen
+    return sentence, add_tokens, special_tokens
+
+
+@preprocess_decorator
+def dataset_preprocess_with_special_tokens(sub, obj, sen):
     """
     [CLS] [S:PER] 이순신 [/S] 장군은 조선 제일의  [O:TITLE] 무신 [/O] 이다.[SEP]
     """
-    sentences, labels = [], []
     add_tokens, special_tokens = [], []
     sub_start_token, sub_end_token = f'[S:{sub["type"]}]', "[/S]"
     obj_start_token, obj_end_token = f'[O:{obj["type"]}]', "[/O]"
@@ -124,30 +114,16 @@ def dataset_preprocess_with_special_tokens(
     temp = sen
     for token in add_index:
         temp = temp[0 : token[0]] + f" {token[1]} " + temp[token[0] :]
-    sentences.append(temp)
-    labels.append(label)
-    if set_AEDA:
-        aug_sentences = AEDA_generator(AEDA, temp, AEDA_cfg.generator)
-        sentences.extend(aug_sentences)
-        labels.extend([label] * len(aug_sentences))
-    return sentences, labels, add_tokens, special_tokens
+    sentence = temp
+    return sentence, add_tokens, special_tokens
 
 
 @preprocess_decorator
-def dataset_with_full_special_tokens(
-    sub,
-    obj,
-    sen,
-    label,
-    set_AEDA=False,
-    AEDA=None,
-    AEDA_cfg=None,
-):
+def dataset_with_full_special_tokens(sub, obj, sen):
     """
     스페셜 토큰을 추가합니다. (꽤 많음..)
     [CLS] [SUB:PER] 이순신 [/SUB:PER] 장군은 조선 제일의  [OBJ:TITLE] 무신 [/OBJ:TITLE] 이다.[SEP]
     """
-    sentences, labels = [], []
     add_tokens, special_tokens = [], []
     sub_start_token, sub_end_token = f'[S:{sub["type"]}]', "[/S]"
     obj_start_token, obj_end_token = f'[O:{obj["type"]}]', "[/O]"
@@ -167,31 +143,17 @@ def dataset_with_full_special_tokens(
     temp = sen
     for token in add_index:
         temp = temp[0 : token[0]] + f" {token[1]} " + temp[token[0] :]
-    sentences.append(temp)
-    labels.append(label)
-    if set_AEDA:
-        aug_sentences = AEDA_generator(AEDA, temp, AEDA_cfg.generator)
-        sentences.extend(aug_sentences)
-        labels.extend([label] * len(aug_sentences))
-    return sentences, labels, add_tokens, special_tokens
+    sentence = temp
+    return sentence, add_tokens, special_tokens
 
 
 @preprocess_decorator
-def dataset_with_least_special_tokens(
-    sub,
-    obj,
-    sen,
-    label,
-    set_AEDA=False,
-    AEDA=None,
-    AEDA_cfg=None,
-):
+def dataset_with_least_special_tokens(sub, obj, sen):
     """
     tokenizer에 따라 sentence를 tokenizing 합니다.
     스페셜 토큰을 추가합니다. ([SUB],[/SUB],[OBJ],[/OBJ] 네 가지)
     [CLS] [SUB] PER @ 이순신 [/SUB] 장군은 조선 제일의 [OBJ] TITLE @ 무신 [/OBJ] 이다.[SEP]
     """
-    sentences, labels = [], []
     add_tokens, special_tokens = [], []
     sub_start_token, sub_end_token = "[SUB]", "[/SUB]"
     obj_start_token, obj_end_token = "[OBJ]", "[/OBJ]"
@@ -208,27 +170,13 @@ def dataset_with_least_special_tokens(
     temp = sen
     for token in add_index:
         temp = temp[: token[0]] + f" {token[1]} " + temp[token[0] :]
-    sentences.append(temp)
-    labels.append(label)
-    if set_AEDA:
-        aug_sentences = AEDA_generator(AEDA, temp, AEDA_cfg.generator)
-        sentences.extend(aug_sentences)
-        labels.extend([label] * len(aug_sentences))
-    return sentences, labels, add_tokens, special_tokens
+    sentence = temp
+    return sentence, add_tokens, special_tokens
 
 
 @preprocess_decorator
-def data_preprocess2(
-    sub,
-    obj,
-    sen,
-    label,
-    set_AEDA=False,
-    AEDA=None,
-    AEDA_cfg=None,
-):
+def data_preprocess2(sub, obj, sen):
     """typed-entity-marker"""
-    sentences, labels = [], []
     add_tokens, special_tokens = [], []
     tokens = [
         (sub["start_idx"], f"[S:{sub['type']}]"),
@@ -247,39 +195,46 @@ def data_preprocess2(
     tokens.sort(key=lambda x: x[0], reverse=True)
     for start_index, token in tokens:
         sen = "".join([sen[:start_index], token, sen[start_index:]])
-    sentences.append(sen)
-    labels.append(label)
-    if set_AEDA:
-        aug_sentences = AEDA_generator(AEDA, sen, AEDA_cfg.generator)
-        sentences.extend(aug_sentences)
-        labels.extend([label] * len(aug_sentences))
-    return sentences, labels, add_tokens, special_tokens
+    sentence = sen
+    return sentence, add_tokens, special_tokens
 
 
 @preprocess_decorator
-def data_preprocess_template(
-    sub,
-    obj,
-    sen,
-    label,
-    set_AEDA=False,
-    AEDA=None,
-    AEDA_cfg=None,
-):
+def data_preprocess_Cap_Token(sub, obj, sen):
+    """
+    [CLS] [SUB:PER] 이순신 [/SUB:PER] 장군은 조선 제일의 [OBJ:TITLE] 무신 [/OBJ:TITLE] 이다.[SEP]
+    스페셜 토큰은 다음과 같이 생성됩니다.
+    ["SUB", "OBJ", "PER", "TITLE" ...]
+    """
+    add_tokens, special_tokens = [], ["SUB", "OBJ", sub["type"], obj["type"]]
+    ###### TODO Your Logic ######
+    sub_start_token, sub_end_token = "[SUB]", "[/SUB]"
+    obj_start_token, obj_end_token = "[OBJ]", "[/OBJ]"
+    add_index = [
+        (sub["start_idx"], sub_start_token + f" {sub['type']} @ "),
+        (sub["end_idx"] + 1, sub_end_token),
+        (obj["start_idx"], obj_start_token + f" {obj['type']} @ "),
+        (obj["end_idx"] + 1, obj_end_token),
+    ]
+    add_index.sort(key=lambda x: x[0], reverse=True)
+    temp = sen
+    for token in add_index:
+        temp = temp[: token[0]] + f" {token[1]} " + temp[token[0] :]
+    sentence = temp
+
+    return sentence, add_tokens, special_tokens
+
+
+@preprocess_decorator
+def data_preprocess_template(sub, obj, sen):
     """
     데이터 전처리 탬플릿입니다. 복붙해서 사용하세요
     만들 데이터 전처리 함수에 대한 설명을 입력해주세요.
     sub, obj는 하나의 객체입니다. 데코레이션 함수에 의해 알아서 for구문 돌립니다.
     """
-    sentences, labels = [], []
     add_tokens, special_tokens = [], []
     ###### TODO Your Logic ######
 
     #############################
-    sentences.append(sen)
-    labels.append(label)
-    if set_AEDA:
-        aug_sentences = AEDA_generator(AEDA, sen, AEDA_cfg.generator)
-        sentences.extend(aug_sentences)
-        labels.extend([label] * len(aug_sentences))
-    return sentences, labels, add_tokens, special_tokens
+    sentence = ""
+    return sentence, add_tokens, special_tokens
