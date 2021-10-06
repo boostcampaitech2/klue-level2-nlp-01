a4f249b86d66822a9ee003e9f7c93390868cdba3
decyma <soos0228@naver.com>
2021-10-06 01:18:14 +0000

    fix: hydra 리로드 오류 수정

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from src.train import train
 from src.inference import inference_main as inference
 
 
-def multimain(filename, cfg):
-    cfg_path = path.join(cfg.dir_path.code, "..", "config")
+def multimain(filename, dir_path):
+    cfg_path = path.join(dir_path, "..", "config")
     hydra.core.global_hydra.GlobalHydra.instance().clear()
 
     @hydra.main(config_path=cfg_path, config_name=filename)
@@ -22,13 +22,14 @@ def multimain(filename, cfg):
 def main(cfg: DictConfig):
     if cfg.multi_config:
         f = []
+        dir_path = cfg.dir_path.code
         for (_, _, filenames) in walk(path.join(cfg.dir_path.code, "..", "config")):
             print(filenames)
             for filename in filenames:
                 if filename[-5:] == ".yaml":
                     f.append(filename)
         for filename in f:
-            multimain(filename[:-5], cfg)
+            multimain(filename[:-5], dir_path)
     else:
         train(cfg)
         if not cfg.hyperparameters.set:
